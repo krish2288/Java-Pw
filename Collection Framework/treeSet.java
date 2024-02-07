c53f615e9bd88afe50ba3c6e3a545a63b75da40f
Krish Singh <krishsingh2288@gmail.com>
2024-02-08 00:02:00 +0530

    lgfhj

--- Collection Framework/treeSet.java
+++ Collection Framework/treeSet.java
@@ -1,8 +1,14 @@
 import java.util.TreeSet;
 
 public class treeSet {
+    // It use binary tree
     public static void main(String[] args) {
         TreeSet ts=new TreeSet();
         ts.add(10);
+        ts.add(30);
+        ts.add(20);
+        ts.add(50);
+        ts.add(40);
+        System.out.println(ts);
     }
 }
